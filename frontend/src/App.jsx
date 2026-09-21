623658708fbc6177c5f320169d8308cff80c2c99
Mehul Suthar <154885299+Mehulsuthar817@users.noreply.github.com>
2026-09-21 12:54:56 +0530

    Comment out projectId in App.jsx
    Comment out the projectId variable in App component.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 // import Visualizer from "./components/Visualizer.jsx";
 
 function App() {
-  const projectId = "6a88b002a0647afb810e5d62";
+  // const projectId = "6a88b002a0647afb810e5d62";
 
   return (
     <>
